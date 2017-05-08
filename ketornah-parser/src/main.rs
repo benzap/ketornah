5b553cdb02239272d13676a856f5c9e0163e7b81
Benjamin Zaporzan <benzaporzan@gmail.com>
2017-05-07 23:37:09 -0400

    correctly parsing file

--- ketornah-parser/src/main.rs
+++ ketornah-parser/src/main.rs
@@ -1,12 +1,16 @@
 #![allow(dead_code)]
+#[macro_use] extern crate lazy_static;
+extern crate regex;
 
 use std::env;
-use std::io::{self, BufReader};
+use std::io::{BufReader};
 use std::io::prelude::*;
 use std::fs::File;
+use regex::Regex;
 
 // Represents pg.30 within SR28 Documentation
 // FOOD_DES.txt
+#[derive(Debug)]
 struct FoodDescription {
     food_databank_number: i32, //NDB_NO [0]
     food_group_category: i32, //FdGrp_Cd [1]
@@ -72,8 +76,16 @@ struct WeightValue {
     gram_weight: f32, //Gm_Wgt [4]
 }
 
+fn parse_field(field: &str) -> String {
+    //let s = field.clone();
+    lazy_static! {
+        static ref RE: Regex = Regex::new("^~?(.*?)~?$").unwrap();
+    }
+    RE.captures(field).map_or("".to_string(), |g| g[1].to_string())
+}
+
 fn read_food_descriptions(file_path: String) -> Vec<FoodDescription> {
-    let v: Vec<FoodDescription> = Vec::new();
+    let mut v: Vec<FoodDescription> = Vec::new();
     let file_handle = File::open(file_path).unwrap();
     let buf_reader = BufReader::new(file_handle);
     for line in buf_reader.lines() {
@@ -81,7 +93,29 @@ fn read_food_descriptions(file_path: String) -> Vec<FoodDescription> {
             Ok(val) => val,
             Err(_) => continue,
         };
-        println!("Line: {}", line);
+        let split = line.split("^");
+        let fields = split.collect::<Vec<&str>>();
+        let food_description = FoodDescription {
+            food_databank_number: parse_field(&fields[0]).parse().unwrap_or(0), //NDB_NO [0]
+            food_group_category: parse_field(&fields[1]).parse().unwrap_or(0), //FdGrp_Cd [1]
+            long_description: parse_field(&fields[2]), //Long_Desc [2]
+            short_description: parse_field(&fields[3]), //Shrt_Desc [3]
+            common_name: parse_field(&fields[4]), //CommName [4]
+            manufacturer: parse_field(&fields[5]), //ManufacName [5]
+            has_nutrient_profile: match parse_field(&fields[6]).as_ref() {
+                "" => false,
+                "Y" => true,
+                _ => false,
+            }, //Survey [6]
+            refuse_description: parse_field(&fields[7]), //Ref_desc [7]
+            refuse_percent: parse_field(&fields[8]).parse().unwrap_or(0), //Refuse [8]
+            scientific_name: parse_field(&fields[9]), //SciName [9]
+            nitrogen_factor: parse_field(&fields[10]).parse::<f32>().unwrap_or(1.0), //N_Factor [10]
+            protein_factor: parse_field(&fields[11]).parse::<f32>().unwrap_or(1.0), //Pro_Factor [11]
+            fat_factor: parse_field(&fields[12]).parse::<f32>().unwrap_or(1.0), //Fat_Factor [12]
+            carbohydrate_factor: parse_field(&fields[13]).parse::<f32>().unwrap_or(1.0), //CHO_Factor [13]  
+        };
+        v.push(food_description);
     }
     v
 }
@@ -103,6 +137,6 @@ fn main() {
     };
     
     let food_descriptions = read_food_descriptions(folder_path + "/FOOD_DES.txt");
-    
+    println!("{:?}", food_descriptions[0]);
 
 }
